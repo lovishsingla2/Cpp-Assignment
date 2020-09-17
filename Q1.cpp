055014694f281b60de7be81bfa8f40977171750c
brandonb82 <71399051+brandonb82@users.noreply.github.com>
2020-09-17 12:16:55 -0700

    Add files via upload

---
+++ Q1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main( ) {
+    int speed {20};
+    
+    int time = {10};
+    
+    int distance;
+    
+    distance = speed * time;
+    
+    std::cout << distance <<std::endl;
+    
+    return 0;
+}

5e36473c81e8d1f30eda387836cf82983173c49e
brandonb82 <71399051+brandonb82@users.noreply.github.com>
2020-09-17 12:18:02 -0700

    Add files via upload

---
+++ A1/Q2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main( ) {
+    double force {172.5};
+    
+    double area {27.5};
+    
+    double pressure;
+    
+    pressure =  area / force;
+    
+    std::cout << pressure << std::endl;
+    
+}
